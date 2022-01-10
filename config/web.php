d1f3bcf2ff71c4317989d1ada009c27f2feac840
smagaev <smagaev@mail.ru>
2022-01-10 15:26:33 +0300

    add txt file with english words

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => 'avtotools/index',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
@@ -14,7 +15,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'jz2pjyb_yqjYZlY381N6EQR_3Q-Zyigw',
+            'cookieValidationKey' => 'jz2pjysdfdsb_yqjYZlY381N6EQR_3Q-Zyigw',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -32,6 +33,29 @@ $config = [
             // 'useFileTransport' to false and configure transport
             // for the mailer to send real emails.
             'useFileTransport' => true,
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+ //               '' => 'home/index',
+//                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
+//                'category/search' => 'category/search',
+//                'category/<id:\d+>/page/<page:\d+>' => 'category/view',
+//                'catalog' => 'catalog/index',
+//                'catalog/toggle' => 'catalog/toggle',
+//                'catalog/search' => 'catalog/search',
+//                'catalog/<slug:.+>' => 'catalog/view',
+//                'my-account' => 'my-info',
+//                'my-info' => 'my-info',
+//                'order' => 'order',
+//
+////                'admin/yii2images/images/image-by-item-and-alias' => 'yii2images/images/image-by-item-and-alias',
+//                'admin/yii2images/images/image-by-item-and-alias' => 'yii2images/images/image-by-item-and-alias',
+//                'product/<slug:.+>' => 'product/view',
+//                'post/<slug:.+>' => 'post/view',
+//                'page/<slug:.+>' => 'page/view',
+            ],
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
