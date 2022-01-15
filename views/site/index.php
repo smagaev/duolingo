3925c40e1d961941e7638922b98fadf92b9c432b
smagaev <smagaev@mail.ru>
2022-01-15 08:19:20 +0300

    home-ended sortin buttons id

--- views/site/index.php
+++ views/site/index.php
@@ -10,8 +10,8 @@ $this->title = 'My Yii Application';
     <div class="body-content text-center">
         <? for ($i = 0; $i < 5; $i++) {
             $id_rand = count($words);
-            $arr_eng[$i] = '<div class="btn my_btn btn-defult border-dark col-sm-4" id="<?= $i ?>"><span class="display">' .  $words[$i]->word .'</span></div>';
-            $arr_tr[$i] = '<div class="btn my_btn btn-default border-dark col-sm-4" id="tr_<?= $i ?>"><span>'. $words[$i]->var1 .'</span></div>';
+            $arr_eng[$i] = '<div class="btn my_btn btn-defult border-dark col-sm-4" id="'. $i .'"><span class="display">' .  $words[$i]->word .'</span></div>';
+            $arr_tr[$i] = '<div class="btn my_btn btn-default border-dark col-sm-4" id="tr_'. $i .'"><span>'. $words[$i]->var1 .'</span></div>';
         }
         shuffle($arr_tr);
 
