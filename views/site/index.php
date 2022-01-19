a510161e30698b4d7fea08faa969b298f40a8749
smagaev <smagaev@mail.ru>
2022-01-19 14:25:53 +0300

    rebuild js script for index page

--- views/site/index.php
+++ views/site/index.php
@@ -7,11 +7,14 @@ $this->title = 'My Yii Application';
     <div class="site-index">
         <br>
         <div class="body-content text-center">
-            <? for ($i = 0; $i < 5; $i++) {
+            <? $n = 1;
+            for ($i = 0; $i < 5; $i++) {
                 $k = $i + 1;
                 $id_rand = count($words);
-                $arr_eng[$i] = '<div class="btn btn-lg my_btn btn_act btn-defult border-dark col-5" data-id="' . $k . '"><span class="display">' . $words[$i]->word . '</span></div>';
-                $arr_tr[$i] = '<div class="btn btn-lg my_btn btn_act btn-default border-dark col-5" data-id="' . $k . '"><span>' . $words[$i]->var1 . '</span></div>';
+                $arr_eng[$i] = '<div id = ' . $n . ' class="btn btn-lg my_btn btn_act btn-defult border-dark col-5" data-id="' . $k . '"><span class="display">' . $words[$i]->word . '</span></div>';
+                $n++;
+                $arr_tr[$i] = '<div id = ' . $n . '  class="btn btn-lg my_btn btn_act btn-default border-dark col-5" data-id="' . $k . '"><span>' . $words[$i]->var1 . '</span></div>';
+                $n++;
             }
             shuffle($arr_tr);
             ?>
@@ -39,62 +42,14 @@ $this->title = 'My Yii Application';
         <br>
         <div class="row">
             <div class="col-sm-4"></div>
-            <div class="btn btn-lg btn-next btn-primary col-sm-4 disabled" onclick="window.location.reload()">Next</div>
+            <div class="btn btn-lg btn-next btn-primary col-sm-4 disabled">Next</div>
             <div class="col-sm-4"></div>
         </div>
     </div>
-<style>
-    /*.my_btn{*/
+    <style>
+        /*.my_btn{*/
         /*font-size:1.2em;*/
         /*fornt-weight :700;*/
-    /*}*/
-</style>
-<? $this->registerJs("
-   
-    $('.my_btn').click(function () {
-               
-                  if(!$('.btn_sel').hasClass('bg-success')){
-                                    $(this).addClass('btn_sel')
-                                           .addClass('bg-success');
-                                            id = $(this).data('id');
-                        } else { 
-                                    if( $(this).data('id') == id){
-                                                    $(this).addClass('bg-success')
-                                                           .addClass('btn_sel');
-                                                    t1 = window.setTimeout(function(){
-                                                           
-                                                           $('.btn_sel').addClass('disabled')
-                                                                        .removeClass('bg-success')
-                                                                        .removeClass('btn_sel')
-                                                                        .removeClass('btn_act')
-                                                                        .unbind();
-                                                    
-                                                    },500);
-                                                
-                                            }else{
-                                                    
-                                                    $('.btn_sel').removeClass('bg-success')
-                                                                 .addClass('bg-danger');
-                                                    $(this).addClass('bg-danger')
-                                                           .addClass('btn_sel');
-                                                    t2 = window.setTimeout(function(){
-                                                    
-                                                            $('.btn_sel').removeClass('bg-danger')
-                                                                         .removeClass('btn_sel')
-                                                                         .removeClass('btn_act');
-                                                    
-                                                    
-                                                    },500);
-                                                    
-                                            }
-                  
-                  }
-          
-//                 if($('.btn_act')[0] === undefined){
-//                 alert('молодцы');
-//                 $('.btn-next').attr('disabled', 'disabled'); 
-//                 }
-//         
-//        
-//    });
-    ", yii\web\View::POS_READY, 'my_btn_handler'); ?>
+        /*}*/
+    </style>
+<? $this->registerJsFile("@web/js/duo.js", ['depends' => [\yii\web\JqueryAsset::class]]); ?>
---
+++ web/js/duo.js
@@ -0,0 +1,55 @@
+$('.my_btn').click(function () {
+    button = $(this);
+    but_select = $('.btn_sel');
+    if (!but_select.hasClass('bg-success')) {
+        button.addClass('btn_sel')
+              .addClass('bg-success');
+        id = button.data('id');
+        n = button.attr('id')
+    }
+    else {
+        if (button.data('id') == id) {
+            if(button.attr('id')!== n ) {
+                button.addClass('bg-success')
+                    .addClass('btn_sel');
+                t1 = window.setTimeout(function () {
+
+                    $('.btn_sel').addClass('disabled')
+                        .removeClass('bg-success')
+                        .removeClass('btn_sel')
+                        .removeClass('btn_act')
+                        .unbind();
+
+                }, 500);
+               if(document.querySelectorAll('.btn_act').length === 2){
+                  $('.btn-next').removeClass('disabled')
+                      .bind('click',function () {
+                          location = "/web/";
+                      })
+               }
+            } else {
+                button.removeClass('bg-success')
+                      .removeClass('btn_sel');
+            }
+        }
+        else {
+
+            $('.btn_sel').removeClass('bg-success')
+                .addClass('bg-danger');
+            button.addClass('bg-danger')
+                .addClass('btn_sel');
+            t2 = window.setTimeout(function () {
+
+                $('.btn_sel').removeClass('bg-danger')
+                    .removeClass('btn_sel');
+
+
+            }, 500);
+
+        }
+
+
+    }
+
+
+});
