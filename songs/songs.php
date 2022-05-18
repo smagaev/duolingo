2d58f271ec95fe8cc06c52b75094d3bcffd1ba95
smagaev <smagaev@mail.ru>
2022-05-18 15:40:13 +0300

    add words

---
+++ songs/songs.php
@@ -0,0 +1,78 @@
+Blue Eyes Unchanged
+
+Across from me, on the bus this afternoon
+A girl of age ninety, eyes of blue
+Peering out from a body changed too soon
+I saw the girl so charming in her youth
+Her hat reveals a woman of taste
+
+Now just to get off this bus is a torturous fate
+She'll slowly make the way back to her place
+An apartment where no one awaits
+Blue eyes unchanged
+
+The body aged
+Blue eyes you are my last witnesses now
+Blue eyes unchanged
+The body aged
+Blue eyes tell me what you wanted to be
+When you grew up
+Blue eyes once reflected in her mother's gaze
+
+On those summers as a child by the lake
+Blue eyes saw love's first embrace
+The boy down the road
+I wonder if he's alive today
+Young girls go laughing past on the road
+
+No one pays attention to the old
+But at least we know justice will be served
+In the end everyone gets their turn
+Blue eyes unchanged
+The body aged
+
+Blue eyes you are my last witnesses now
+Blue eyes unchanged
+The body aged
+Blue eyes tell me what you wanted to be
+When you grew up
+
+
+Голубые глаза не изменились
+Напротив меня, в автобусе сегодня после полудня
+голубоглазая девочка лет девяноста
+Глаза на теле, изменившемся слишком быстро
+Я видела эту девочку такой очаровательной в юности.
+
+Ее шляпа выдает женщину со вкусом,
+а нынче ей просто выйти из автобуса-это мучительный удел
+Она медленно вернется к себе домой,
+в квартиру, где никто не ждет.
+
+Голубые глаза не изменились,
+лишь тело состарилось,
+голубые глаза, теперь вы мои последние свидетели.
+Голубые глаза не изменились,
+лишь тело состарилось,
+голубые глаза говорят мне, какой ты хотела быть
+когда росла
+
+Голубые глаза когда-то отражались во взгляде ее матери
+в летние дни детства у озера.
+Голубые глаза увидели первое любовное объятье,
+мальчик на дороге...
+Интересно, жив ли он сегодня?
+
+Девушки идут смеясь мимо по дороге,
+никто не обращает внимания на старость,
+но, по крайней мере, мы знаем, что справедливость восторжествует.
+в конце концов, каждый встанет в эту очередь.
+
+Голубые глаза не изменились,
+лишь тело состарилось,
+голубые глаза, теперь вы мои последние свидетели.
+Голубые глаза не изменились,
+лишь тело состарилось,
+голубые глаза говорят мне, какой ты хотела быть
+когда росла
+https://lyricstranslate.com
