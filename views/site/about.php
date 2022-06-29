e9d92b67de4dd8f8efba1e053e1ce94590758e71
smagaev <smagaev@mail.ru>
2022-06-29 14:12:47 +0300

    about on russian and gemany

--- views/site/about.php
+++ views/site/about.php
@@ -9,10 +9,14 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        While self-studying English on the Internet, I came across the site <a href="https://duolingo.com">duolingo.com</a>, which I really liked! I have been studying on this site for a year and have almost completed the entire English course. Everything is good here, but for a very long time. According to the recommendations, it is desirable to write out the studied words for better assimilation. What I did! However! Repeating them, as practice has shown, is not very convenient, and it is pointless. Already learned words constantly come across, others, not studied, come across rarely. It would be nice to sift out the studied material! Therefore, in order to simplify the memorization of words and speed up the repetition of the material covered, I wrote a small free program, which I want to bring to your attention.
     </p>
-
-    <code><?= __FILE__ ?></code>
+    <p>
+    The site allows you not only to study the words I have collected, but also the words that you add yourself! Added languages can be not only in English and Russian, but also in any other. True, the site interface is presented only in Russian, German and English.
+    To add your own words, view statistics, and exclude learned words from the learning process, you need to register on the site.
+    </p>
+    <p>
+        If you have any questions or wishes, you can leave a message on the page <a href="/contact">Contacts</a>
+    </p>
 </div>
--- views/site/de/about.php
+++ views/site/de/about.php
@@ -9,10 +9,28 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        Beim Selbstlernen der englischen Sprache im Internet bin ich auf die Seite <a href="https://duolingo.com">duolingo.com<a/> gestoßen, die mir sehr
+        gut gefallen hat! Ich studiere seit einem Jahr auf dieser Seite und habe fast den gesamten Englischkurs
+        abgeschlossen. Alles ist gut hier, aber für eine sehr lange Zeit. Gemäß den Empfehlungen ist es wünschenswert,
+        die untersuchten Wörter zur besseren Assimilation aufzuschreiben. Was ich getan habe! Jedoch! Sie zu
+        wiederholen, wie die Praxis gezeigt hat, ist nicht sehr bequem und sinnlos. Bereits gelernte Wörter stoßen
+        ständig auf andere, nicht gelernte, selten. Es wäre schön, das untersuchte Material auszusieben! Um das
+        Auswendiglernen von Wörtern zu vereinfachen und die Wiederholung des behandelten Materials zu beschleunigen,
+        habe ich daher ein kleines kostenloses Programm geschrieben, auf das ich Sie aufmerksam machen möchte.
     </p>
-
-    <code><?= __FILE__ ?></code>
+    <p>
+        Auf der Seite können Sie nicht nur die von mir gesammelten Wörter studieren, sondern auch die Wörter, die Sie
+        selbst
+        hinzufügen! Hinzugefügte Sprachen können nicht nur in Englisch und Russisch sein, sondern auch in jeder anderen.
+        Die
+        Benutzeroberfläche der Website wird zwar nur in Russisch, Deutsch und Englisch angezeigt.
+    </p>
+    <p>
+        Um Ihre eigenen Wörter hinzuzufügen, Statistiken anzuzeigen und gelernte Wörter vom Lernprozess auszuschließen,
+        müssen Sie sich auf der Website registrieren.
+    </p>
+    <p>
+        Wenn Sie Fragen oder Wünsche haben, können Sie auf der Seite <a href="/contact/">Kontakte</a> eine Nachricht hinterlassen
+    </p>
 </div>
