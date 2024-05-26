026c119a1da22dac7648a521cde38763adcd4a09
smagaev <smagaev@mail.ru>
2024-05-26 11:25:29 +0300

    Verbs.php

---
+++ web/englishRules/Verbs.php
@@ -0,0 +1,18 @@
+
+<h1>Verbs</h1>
+Let’s talk about Phrasal Verbs!
+Some
+verbs
+ are made of two words.
+You can get out of the car.
+We can get around on our bikes.
+Some two‑word
+verbs
+ don’t need to stay together.
+I need to pick up my son.
+I need to pick my son up.
+Words like it, him, etc. must go between the two parts of the
+verb
+!
+I need to pick him up.
+I need to drop her off.
