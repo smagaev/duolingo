20142162b52cc338d4d159ed218e9e9561e8b9fa
smagaev <smagaev@mail.ru>
2024-06-08 12:38:59 +0300

    add rules verbs

---
+++ web/englishRules/About verbs.php
@@ -0,0 +1,26 @@
+<h1>Let's talk about Verbs!</h1>
+
+<p>After <strong>like</strong>, <strong>love</strong> and <strong>hate</strong>, you can use <span class="text-primary">to</span> with the <span class="text-primary">verb</span>,
+    or just the -ing form of the verb.</p>
+<ul>
+    <li>I like <div class="text-primary">to sing</div></li>
+    <li>I like <div class="text-primary">singing</div></li>
+</ul>
+
+<p>After some verbs, like <strong>need</strong> and <strong>want</strong>,
+you only use the <span class="text-primary">to</span> form.
+</p>
+
+<ul>
+    <li>I need <div class="text-primary">to talk</div> to you tomorrow.</li>
+    <li>I want <div class="text-primary">to cook</div>dinner.</li>
+</ul>
+
+<p>After other verbs, like <strong>enjoy</strong><strong>keep</strong> and <strong>avoid</strong>,
+you only use the <span class="text-primary">-ing</span> form.
+</p>
+
+<ul>
+    <li>Do you enjoy <div class="text-primary">singing</div>?</li>
+    <li>Laura keeps <div class="text-primary">calling</div> me.</li>
+</ul>
