7a23afbccea16dd2eafb6a709e191015f0d3c9d6
smagaev <smagaev@mail.ru>
2024-06-09 18:16:41 +0300

    new rule phrasal verbs

---
+++ web/englishRules/Phrasal verbs.php
@@ -0,0 +1,13 @@
+<h1>Let's talk about Phrasal Verbs!</h1>
+<p>Some verbs are made of two words. Here are some examples:</p>
+
+<ul>
+    <li>You can <div class="text-primary">get out</div> of the car.
+    </li>
+    <li>We <div class="text-primary">get stuck</div> in traffic almost everyday.
+    </li>
+    <li>She usually <div class="text-primary">gets around</div> the city on her bike.
+    </li>
+    <li>She is <div class="text-primary">getting rid</div> of her old car.
+    </li>
+</ul>
