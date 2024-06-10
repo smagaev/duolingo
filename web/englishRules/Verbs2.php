d0c896ed68655eed497289a991631fe9c0d22dfe
smagaev <smagaev@mail.ru>
2024-06-10 19:37:38 +0300

    *

---
+++ web/englishRules/Verbs2.php
@@ -0,0 +1,15 @@
+<h1>Let's talk about Verbs!</h1>
+
+<h3>After <strong>want, need, ask,</strong> or <strong>tell</strong>,
+    use <span class="text-primary">to</span> with the verb.</h3>
+<ul>
+    <li>She wants them <span class="text-primary">to buy</span> a car.</li>
+    <li>Do you need me <span class="text-primary">to cook</span> dinner?</li>
+</ul>
+
+<h3>After <strong>let</strong> and <strong>make</strong>, use the base form without <strong>to</strong>.</h3>
+
+<ul>
+    <li>My teacher makes me <span class="text-primary">read</span> me a lot.</li>
+    <li>I let him <span class="text-primary">play</span> tennis.</li>
+</ul>
