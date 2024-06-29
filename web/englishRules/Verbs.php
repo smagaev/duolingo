f69498772985da5136330eb9861eccac64a39ffa
smagaev <smagaev@mail.ru>
2024-06-29 11:41:37 +0300

    add rules english

--- web/englishRules/Verbs.php
+++ web/englishRules/Verbs.php
@@ -1,18 +1,22 @@
+<h2>Let’s talk about Phrasal Verbs!</h2>
+<p>&nbsp;</p>
 
-<h1>Verbs</h1>
-Let’s talk about Phrasal Verbs!
-Some
-verbs
- are made of two words.
-You can get out of the car.
-We can get around on our bikes.
-Some two‑word
-verbs
- don’t need to stay together.
-I need to pick up my son.
-I need to pick my son up.
-Words like it, him, etc. must go between the two parts of the
-verb
-!
-I need to pick him up.
-I need to drop her off.
+<h4>Some verbs  are made of two words.</h4>
+
+<ul>
+<li>You can <strong>get out</strong>of the car.
+</li><li>We can <stong>get around</stong> on our bikes.</li>
+</ul>
+
+<h4>Some two‑word verbs don’t need to stay together.</h4>
+
+<ul><li>
+        I need to pick up my son.</li><li>
+        I need to pick my son up.</li>
+</ul>
+<h4>
+    Words like it, him, etc. must go between the two parts of the verb!</h4>
+<ul>
+    <li>I need to pick him up.</li>
+    <li>I need to drop her off.</li>
+</ul>
--- web/englishRules/there is, are - have, has.php
+++ web/englishRules/there is, are - have, has.php
@@ -1,13 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 02.09.2023
- * Time: 16:42
- */
-
-
-?>
 
 <p><strong>there is \are</strong> употребляем, когда описываем какие предметы, вещи или дюди находятся в каком-то месте.</p>
 <p><strong>have\has</strong> - когда говорим о том, что имеем мы.</p>
